3f30e7f36792320e1662d04b86537b1629911dc3
Coder <basia.bugi.coding@icloud.com>
2023-07-31 10:44:15 +0200

    Altered post model. Migrated

--- project4/network/models.py
+++ project4/network/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='usersPosts')
     body = models.TextField()
     likes = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
