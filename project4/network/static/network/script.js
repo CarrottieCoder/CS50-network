b885567d6ca2d801b910ed62ae91a301ea9f3e47
Coder <basia.bugi.coding@icloud.com>
2023-08-02 15:25:16 +0200

    Working edit-post (and cancel)

--- project4/network/static/network/script.js
+++ project4/network/static/network/script.js
@@ -13,13 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
           });
     }
     document.querySelectorAll('.edit-post').forEach((link) => {
-        link.onclick = function() {
-            if(!text.includes('<textarea')){
-                postParent.innerHTML = " "
-                
-                const edit_Textarea  = document.createElement('textarea')
+        link.onclick = function() {  
+            const post_text = link.parentElement.childNodes[1]
+            const text = post_text.innerHTML
+            if (!localStorage.getItem(post_text.id)){
+                localStorage.setItem(post_text.id, text);
+            }
+            console.log(post_text)
 
-                // Style the textarea
+            //Check if you're canceling or editing
+            if (!text.includes("<textarea")){
+                // Create textarea
+                const edit_Textarea = document.createElement('textarea')
                 edit_Textarea.id = 'edit-textarea'
                 edit_Textarea.style.width = "100%" 
                 edit_Textarea.rows = 8
@@ -31,20 +36,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 submit_button.className = "btn btn-primary"
                 submit_button.innerHTML = "Save"
 
+                //Clear div
+                post_text.innerHTML = " "
+                
                 //Add elements to the document
-                postParent.append(edit_Textarea)
-                postParent.append(submit_button)
+                post_text.append(edit_Textarea)
+                post_text.append(submit_button)
 
-                document.querySelector('#submit-button').onclick = edit_post(post)
+                link.textContent = "cancel"
+            } else{
+                post_text.innerHTML = localStorage.getItem(post_text.id);
+                localStorage.removeItem(post_text.id);
+                link.textContent = "edit"
+            }
 
-                post.innerHTML = "cancel"
-            }
-            else {
-                const edited_text = document.querySelector('#edit-textarea').value
-                console.log(edited_text)
-                postParent.innerHTML = edited_text
-                post.innerHTML = "edit"
-            }
         };
     });
     
